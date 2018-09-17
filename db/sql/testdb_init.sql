585449b212abc5bc146f2e84f56f7948bce2bd24
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-17 09:17:35 +0900

    MySQL-UTF8対応、drop database追加

--- db/sql/testdb_init.sql
+++ db/sql/testdb_init.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS ofpp;
 CREATE DATABASE ofpp DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'ofpp'@'localhost' IDENTIFIED BY '#ofpp#';
 GRANT ALL PRIVILEGES ON ofpp.* TO 'ofpp'@'localhost';
